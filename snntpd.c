b2af07365f0e910cc3a5364448ca90c154d36d0d
liu-chong <liuchong14@gmail.com>
2011-08-07 13:51:11 +0800

    Fix char '©', 'å'

--- snntpd.c
+++ snntpd.c
@@ -1,8 +1,8 @@
 /*
  * This file is part of the sn package.
  * Distribution of sn is covered by the GNU GPL. See file COPYING.
- * Copyright © 1998-2000 Harold Tay.
- * Copyright © 2000- Patrik Rådman.
+ * Copyright Â© 1998-2000 Harold Tay.
+ * Copyright Â© 2000- Patrik RÃ¥dman.
  */
 
 /*
